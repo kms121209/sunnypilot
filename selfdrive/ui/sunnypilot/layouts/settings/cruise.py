201021f0959092c72360b1c5c605dd859fc54a35
nayan <nayan8teen@gmail.com>
2026-02-05 23:03:13 -0500

    init cruise panel

--- selfdrive/ui/sunnypilot/layouts/settings/cruise.py
+++ selfdrive/ui/sunnypilot/layouts/settings/cruise.py
@@ -4,10 +4,17 @@ Copyright (c) 2021-, Haibin Wen, sunnypilot, and a number of other contributors.
 This file is part of sunnypilot and is licensed under the MIT License.
 See the LICENSE.md file in the root directory for more details.
 """
+from enum import IntEnum
+
 from openpilot.common.params import Params
+from openpilot.system.ui.lib.multilang import tr
+from openpilot.system.ui.sunnypilot.widgets.list_view import toggle_item_sp, option_item_sp, simple_button_item_sp
 from openpilot.system.ui.widgets.scroller_tici import Scroller
 from openpilot.system.ui.widgets import Widget
 
+class PanelType(IntEnum):
+  CRUISE = 0
+  SLA = 1
 
 class CruiseLayout(Widget):
   def __init__(self):
@@ -19,12 +26,74 @@ class CruiseLayout(Widget):
 
   def _initialize_items(self):
 
+    self.icbm_toggle = toggle_item_sp(
+      title=tr("Intelligent Cruise Button Management (ICBM) (Alpha)"),
+      description="",
+      param="IntelligentCruiseButtonManagement")
+
+    self.scc_v_toggle = toggle_item_sp(
+      title=tr("Smart Cruise Control - Vision"),
+      description=tr("Use vision path predictions to estimate the appropriate speed to drive through turns ahead."),
+      param="SmartCruiseControlVision")
+
+    self.scc_m_toggle = toggle_item_sp(
+      title=tr("Smart Cruise Control - Map"),
+      description=tr("Use map data to estimate the appropriate speed to drive through turns ahead."),
+      param="SmartCruiseControlMap")
+
+    self.custom_acc_toggle = toggle_item_sp(
+      title=tr("Custom ACC Speed Increments"),
+      description="",
+      param="CustomAccIncrementsEnabled")
+
+    self.custom_acc_short_increment = option_item_sp(
+      title=tr("Short Press Increment"),
+      param="CustomAccShortPressIncrement",
+      min_value=1, max_value=10, value_change_step=1,
+      inline=True)
+
+    self.custom_acc_long_increment = option_item_sp(
+      title=tr("Long Press Increment"),
+      param="CustomAccLongPressIncrement",
+      value_map={1: 1, 2: 5, 3: 10},
+      min_value=1, max_value=3, value_change_step=1,
+      inline=True)
+
+    self.sla_settings_button = simple_button_item_sp(
+      button_text=lambda: tr("Speed Limit"),
+      button_width=800,
+      callback=lambda: self._set_current_panel(PanelType.SLA)
+    )
+
+    self.dec_toggle = toggle_item_sp(
+      title=tr("Enable Dynamic Experimental Control"),
+      description=tr("Enable toggle to allow the model to determine when to use sunnypilot ACC or sunnypilot End to End Longitudinal."),
+      param="DynamicExperimentalControl")
+
     items = [
+      self.icbm_toggle,
+      self.scc_v_toggle,
+      self.scc_m_toggle,
+      self.custom_acc_toggle,
+      self.custom_acc_short_increment,
+      self.custom_acc_long_increment,
+      self.sla_settings_button,
+      self.dec_toggle
     ]
     return items
 
   def _render(self, rect):
-    self._scroller.render(rect)
+    if self._current_panel == PanelType.SLA:
+      return
+    else:
+      self._scroller.render(rect)
 
   def show_event(self):
+    self._set_current_panel(PanelType.CRUISE)
     self._scroller.show_event()
+
+  def _set_current_panel(self, panel: PanelType):
+    self._current_panel = panel
+
+  def _update_state(self):
+    super()._update_state()
